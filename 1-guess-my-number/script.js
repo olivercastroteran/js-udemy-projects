06061c4d4e501f6e83f655b22cfd5ed3c5a99f20
oliver <olivercastroteran@gmail.com>
2022-07-04 23:22:02 -0400

    game logic implemented

--- 1-guess-my-number/script.js
+++ 1-guess-my-number/script.js
@@ -1,3 +1,35 @@
 'use strict';
 
-const msg = document.querySelector('.message').textContent;
+const msg = document.querySelector('.message');
+const checkBtn = document.querySelector('.check');
+
+const num = Math.trunc(Math.random() * 20) + 1;
+const secretNumber = (document.querySelector('.number').textContent = num);
+let score = 20;
+
+checkBtn.addEventListener('click', () => {
+  const guess = +document.querySelector('.guess').value;
+  if (!guess) {
+    msg.textContent = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    msg.textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      msg.textContent = '📈 Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      msg.textContent = '💩 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      msg.textContent = '📉 Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      msg.textContent = '💩 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
